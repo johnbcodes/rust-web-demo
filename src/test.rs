b7226c55da6658a6e32ce70b80a21d8fddd2e062
John B Codes <59550+johnbcodes@users.noreply.github.com>
2022-04-22 11:55:57 -0400

    Run completely experimental code in separate project

--- src/test.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    use regex::Regex;
-    let re = Regex::new(r"(?i)abb").unwrap();
-    let result = re.replace_all("Abbot", "xxx");
-    println!("{}", result); // => "xxxxx xxxxx!"
-}
