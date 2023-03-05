7864abb70b6e6de9b9197d3469834b6b4a985c0a
John Bledsoe <59550+johnbcodes@users.noreply.github.com>
2023-03-05 11:19:23 -0500

    feature(src): Record duration of page DB interaction

--- src/scroll.rs
+++ src/scroll.rs
@@ -6,6 +6,7 @@ use axum::{
     response::IntoResponse,
 };
 use sqlx::SqlitePool;
+use std::time::Instant;
 use tracing::info;
 
 pub(crate) async fn index() -> impl IntoResponse {
@@ -20,8 +21,11 @@ pub(crate) async fn page(
     pagination: Option<Query<people::Pagination>>,
     State(pool): State<SqlitePool>,
 ) -> impl IntoResponse {
+    let start = Instant::now();
     let Query(pagination) = pagination.unwrap_or_default();
     let records = people::load(&pool, &pagination).await;
+    let duration = start.elapsed().as_micros();
+    info!("DB duration: {duration} μs");
     let total = records.len();
     info!("Records: ({total})");
     PageTemplate {
