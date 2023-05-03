7fc5d81e67cd100dea1d6d5452bf39ca9ff1b88f
John Bledsoe <59550+johnbcodes@users.noreply.github.com>
2023-05-03 10:52:14 -0400

    feat: use environment variables and build script to dynamically specify Parcel-generated frontend asset files to be embedded in executable

---
+++ build.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+fn main() {
+    let mut found_css = false;
+    let mut found_favicon = false;
+    let mut found_script = false;
+    match fs::read_dir("ui/target/public") {
+        Ok(dir) => {
+            for entry in dir {
+                let path = entry.unwrap().path();
+                let extension = path.extension().unwrap();
+                let file_name = path.file_name().unwrap().to_str().unwrap();
+                if extension == "css" {
+                    println!("cargo:rerun-if-changed={}", path.display());
+                    println!("cargo:rustc-env=STYLESHEET={}", file_name);
+                    found_css = true;
+                }
+                if extension == "ico" {
+                    println!("cargo:rerun-if-changed={}", path.display());
+                    println!("cargo:rustc-env=FAVICON={}", file_name);
+                    found_favicon = true;
+                }
+                if extension == "js" {
+                    println!("cargo:rerun-if-changed={}", path.display());
+                    println!("cargo:rustc-env=SCRIPT={}", file_name);
+                    found_script = true;
+                }
+            }
+        }
+        Err(_) => panic!("Parcel output directory not found. Please execute `npm run build`"),
+    }
+
+    let mut panics: Vec<&str> = vec![];
+    if !found_css {
+        panics.push("Did not find CSS!!");
+    }
+    if !found_favicon {
+        panics.push("Did not find favicon!!");
+    }
+    if !found_script {
+        panics.push("Did not find script!!");
+    }
+    if !panics.is_empty() {
+        panic!("{}", panics.join("\n"));
+    }
+}
